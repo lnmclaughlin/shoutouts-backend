fa28ced201233c2eccee78ff4e7932caa08aa96d
Laurie McLaughlin <lnmclaughlin@gmail.com>
2022-06-09 16:37:30 -0400

    initial commit

--- functions/src/models/ShoutOut.ts
+++ functions/src/models/ShoutOut.ts
@@ -1,8 +1,15 @@
 import { ObjectId } from "mongodb";
 
+export interface User {
+  displayName: string;
+  uid: string;
+}
 export default interface ShoutOut {
   _id?: ObjectId;
   to: string;
   from: string;
   text: string;
+  photoURL?: string;
+  image?: string;
+  likes?: User[];
 }
--- functions/src/routes/shoutOutRouter.ts
+++ functions/src/routes/shoutOutRouter.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import { ObjectId } from "mongodb";
 import { getClient } from "../db";
-import ShoutOut from "../models/ShoutOut";
+import ShoutOut, { User } from "../models/ShoutOut";
 
 const shoutOutRouter = express.Router();
 
@@ -23,12 +24,80 @@ shoutOutRouter.get("/", async (req, res) => {
   }
 });
 
+shoutOutRouter.get("/to/:to", async (req, res) => {
+  const to: string = req.params.to;
+  try {
+    const client = await getClient();
+    const results = await client
+      .db()
+      .collection<ShoutOut>("shoutouts")
+      .find({ to })
+      .toArray();
+    res.json(results);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+shoutOutRouter.get("/me/:me", async (req, res) => {
+  const me: string = req.params.me;
+
+  try {
+    const client = await getClient();
+    const results = await client
+      .db()
+      .collection<ShoutOut>("shoutouts")
+      .find({ $or: [{ to: me }, { from: me }] })
+      .toArray();
+    res.json(results);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+shoutOutRouter.put("/upvote/:id", async (req, res) => {
+  const id: string = req.params.id;
+  const user: User = req.body;
+  try {
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<ShoutOut>("shoutouts")
+      .updateOne({ _id: new ObjectId(id) }, { $push: { likes: user } });
+    if (result.modifiedCount === 0) {
+      res.status(404).json({ message: "Id not Found" });
+    } else {
+      res.status(201).json(user);
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
 shoutOutRouter.post("/", async (req, res) => {
   const newShoutOut: ShoutOut = req.body;
   try {
     const client = await getClient();
     await client.db().collection<ShoutOut>("shoutouts").insertOne(newShoutOut);
     res.status(201).json(newShoutOut);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+shoutOutRouter.delete("/:id", async (req, res) => {
+  const id: string = req.params.id;
+  try {
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<ShoutOut>("shoutouts")
+      .deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) {
+      res.status(404).json({ message: `shoutout with id: ${id} not found` });
+    } else {
+      res.sendStatus(204);
+    }
   } catch (err) {
     errorResponse(err, res);
   }
